64d129af6dd887d4a553be4145e1845cb4b665cd
Tomi Hukkalainen <tomi.hukkalainen@iki.fi>
2016-04-03 18:31:44 +0300

    Initial commit

---
+++ menu.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from base64 import b64decode
+from os import listdir, path
+from re import findall
+from subprocess import PIPE, Popen
+
+from appdirs import user_config_dir
+import cmd2
+from ruamel import yaml
+
+from six.moves import input
+
+# Python readline has completer delimiters that work for the Python shell,
+# but we want to complete names that have dashes
+import readline
+readline.set_completer_delims(readline.get_completer_delims().replace('-', ''))
+
+
+CONFIG_PATH = path.join(user_config_dir('dt-openshift-deploy', 'digitransit'),
+                        'dt-openshift-deploy.yaml')
+OS_TYPES = ('svc', 'dc', 'route')
+
+
+class DeployShell(cmd2.Cmd, object):
+    intro = 'Type help or ? to list commands.\n'
+
+    def __init__(self, config):
+        self.config = config
+        super(DeployShell, self).__init__()
+        self.prompt = self.colorize('(dt-openshift-deploy) ', 'bold')
+
+    def oc_cmd(self, *args):
+        '''Run oc with given arguments'''
+        p = Popen((self.config['oc_path'],) + args, stderr=PIPE)
+        if p.wait() != 0:
+            errors = '\n'.join(p.stderr.readlines())
+            if errors.find('provide credentials') != -1:
+                print('You need to log in')
+                return False
+            else:
+                print(errors)
+        return True
+
+    def preloop(self):
+        self.do_status(None)
+        print('\n')
+
+    def target_complete(self, text='', line='', begidx=None, endidx=None):
+        '''
+        Complete first arg to OS types and second to available yamls of given type.
+        '''
+        spaces = len(findall(' ', line))
+        if spaces < 2:
+            choices = OS_TYPES
+        elif spaces == 2:
+            choices = dirswith(line.split()[1]) + ['all']
+        else:
+            choices = []
+        return [x + ' ' for x in choices if x.startswith(text)]
+
+    def do_status(self, arg):
+        '''Get oc status (printed at startup)'''
+        # TODO List currently deployed repository shas, and compare to :latest
+        #      (or some other tag, if used) in Docker Hub
+        # TODO Test that routes/services are actually reachable
+        self.oc_cmd('status')
+
+    def do_recreate(self, arg):
+        '''recreate TYPE NAME
+
+        Delete and create an OpenShift resource from files under subfolders.
+        Files need to be named TYPE.yaml.
+
+        NAME can be `all` to recreate all targets of given type
+
+        Examples:
+            recreate svc digitransit-proxy
+            recreate dc all
+        '''
+        type, target = arg.split()
+        if target == 'all':
+            targets = dirswith(type)
+        else:
+            targets = [target]
+        for t in targets:
+            # TODO delete should take the target from the name field in yaml
+            #      as that might not match the dir name
+            if not self.oc_cmd('delete', type, t):
+                return
+            if type == 'dc':
+                # This also deletes istags
+                self.oc_cmd('delete', 'is', t)
+
+            self.oc_cmd('create', '-f', path.join(t, type + '.yaml'))
+
+    complete_recreate = target_complete
+
+    def do_deploy_image(self, arg):
+        '''Deploy a new image from Docker Hub to OS registry.'''
+        # TODO Should get the repositories (Docker image name) from within the yamls
+        # TODO Hardcoded hsldevcom
+        if arg == 'all':
+            targets = dirswith(type)
+        else:
+            targets = [arg]
+        for t in targets:
+            self.oc_cmd('import-image', 'hsldevcom/' + t)
+
+    def complete_deploy_image(self, text='', line='', begidx=None, endidx=None):
+        choices = dirswith('dc') + ['all']
+        return [x + ' ' for x in choices if x.startswith(text)]
+
+    def do_login(self, arg):
+        '''login USERNAME PASSWORD
+
+        Login to OpenShift instance with the oc client (creating an OAuth token)
+        '''
+        username, password = arg.split()
+        self.oc_cmd('login', '--username', username, '--password', password)
+
+    def do_secrets(self, arg):
+        '''
+        secrets
+            List all non-kubernets b64 encoded secrets
+        secrets clear
+            List cleartext secrets
+        '''
+        if arg == 'clear':
+            clear = True
+        else:
+            clear = False
+
+        p = Popen([self.config['oc_path'], 'get', 'secrets', '-o', 'yaml'],
+                  stdout=PIPE)
+        p.wait()
+        result = yaml.safe_load(p.stdout)
+        for secret in (r for r in result['items']
+                       if not r['type'].startswith('kubernetes')):
+            print(secret['metadata']['name'])
+            for k, v in secret['data'].iteritems():
+                if clear:
+                    v = b64decode(v)
+                print(' %s: %s' % (k, v))
+
+    def complete_secrets(self, text='', line='', begidx=None, endidx=None):
+        return ['clear']
+
+    def do_new_secret(self, arg):
+        '''new_secret NAME KEY VALUE'''
+        name, key, value = arg.split()
+        self.oc_cmd('create', 'secret', 'generic', name,
+                    '--from-literal=' + key + '=' + value)
+
+    def do_eof(self, arg):
+        print()  # Clear line, like when quitting with quit
+        return True
+
+
+def dirswith(filename):
+    '''Get all directories containing given .yaml file.'''
+    return sorted([x for x in listdir('.')
+                   if (path.isdir(x) and
+                       not x.startswith('.') and
+                       path.exists(path.join(x, filename + '.yaml')))])
+
+
+def read_config():
+    config = {}
+    if path.exists(CONFIG_PATH):
+        with open(CONFIG_PATH) as f:
+            config.update(yaml.safe_load(f))
+    return config
+
+
+def save_config(config):
+    if not path.exists(path.dirname(CONFIG_PATH)):
+        from os import makedirs
+        makedirs(path.dirname(CONFIG_PATH))
+    with open(CONFIG_PATH, 'w') as f:
+        yaml.safe_dump(config, f)
+
+
+def main():
+    config = read_config()
+    if 'oc_path' not in config:
+        config['oc_path'] = path.expanduser(input("Enter path to oc binary: "))
+        save_config(config)
+
+    DeployShell(config).cmdloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+requirements = [
+    'appdirs',
+    'cmd2',
+    'ruamel.yaml',
+    'six'
+]
+
+setup(
+    name='digitransit-openshift',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='Control Digitransit OpenShift environment',
+
+    # The project's main homepage.
+    url='http://digitransit.fi',
+
+    # Author details
+    author='HSL / Digitransit',
+    author_email='digitransit@hsl.fi',
+
+    # Choose your license
+    license='EUPL/AGPLv3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 2 - Pre-Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: European Union Public Licence 1.1 (EUPL 1.1)',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities'
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    #packages=find_packages(),
+
+    py_modules=['menu'],
+
+
+    install_requires=requirements,
+    setup_requires=requirements,
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        # 'dev': ['check-manifest'],
+        # 'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'dt_os_menu=menu:main',
+        ],
+    },
+)
